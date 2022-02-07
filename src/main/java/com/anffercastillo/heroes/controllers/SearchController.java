ca5e2af66f96e92c39395fe1f4ab479af7e0933a
Anffer Castillo <anffer.castillo@gmail.com>
2022-02-07 19:39:00 -0300

    SearchController bad request (empty param)

--- src/main/java/com/anffercastillo/heroes/controllers/SearchController.java
+++ src/main/java/com/anffercastillo/heroes/controllers/SearchController.java
@@ -2,10 +2,14 @@ package com.anffercastillo.heroes.controllers;
 
 import com.anffercastillo.heroes.dto.SearchResponse;
 import com.anffercastillo.heroes.services.HeroesService;
+import com.anffercastillo.heroes.utils.MessagesConstants;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/search")
@@ -20,6 +24,10 @@ public class SearchController {
   @GetMapping
   public SearchResponse getHeroesByName(@RequestParam(required = true) String term)
       throws Exception {
+    if (!StringUtils.hasLength(term)) {
+      throw new ResponseStatusException(
+          HttpStatus.BAD_REQUEST, MessagesConstants.HERO_EMPTY_NAME_ERROR);
+    }
     var results = heroesService.getHeroesByName(term);
     return SearchResponse.buildResponse(results);
   }

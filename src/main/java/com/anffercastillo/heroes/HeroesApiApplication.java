83c06c031b7729f36b9b19b9ade7d11a23bf2e8a
Anffer Castillo <anffer.castillo@gmail.com>
2022-02-09 23:39:45 -0300

    User Detail object not needed

--- src/main/java/com/anffercastillo/heroes/HeroesApiApplication.java
+++ src/main/java/com/anffercastillo/heroes/HeroesApiApplication.java
@@ -2,12 +2,13 @@ package com.anffercastillo.heroes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class HeroesApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HeroesApiApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(HeroesApiApplication.class, args);
+  }
 }
--- src/main/java/com/anffercastillo/heroes/services/HeroesService.java
+++ src/main/java/com/anffercastillo/heroes/services/HeroesService.java
@@ -1,15 +1,19 @@
 package com.anffercastillo.heroes.services;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
 import com.anffercastillo.heroes.dto.HeroDTO;
 import com.anffercastillo.heroes.dto.HeroRequest;
 import com.anffercastillo.heroes.repositories.HeroesRepository;
 import com.anffercastillo.heroes.utils.MessagesConstants;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 @Service
 public class HeroesService {
@@ -20,26 +24,57 @@ public class HeroesService {
     this.heroesRepository = heroRepositoy;
   }
 
+  @Cacheable("heroes")
   public HeroDTO getHero(long id) {
-    // TODO: change this to a proper exception with 404
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
     return heroesRepository
         .findHeroesById(id)
         .map(HeroDTO::buildHeroDTO)
         .orElseThrow(() -> new NoSuchElementException(MessagesConstants.HERO_NOT_FOUND));
   }
 
+  @Cacheable("search")
   public List<HeroDTO> getHeroes() {
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
     return heroesRepository.findAll().stream()
         .map(HeroDTO::buildHeroDTO)
         .collect(Collectors.toList());
   }
 
+  @Caching(
+      evict = {
+        @CacheEvict(value = "heores", key = "#a0"),
+        @CacheEvict(value = "search", allEntries = true)
+      })
   public void deleteHero(long id) {
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
     var hero = getHero(id);
     heroesRepository.deleteById(hero.getId());
   }
 
+  @Caching(
+      evict = {
+        @CacheEvict(value = "heores", key = "#a0"),
+        @CacheEvict(value = "search", allEntries = true)
+      })
   public HeroDTO updateHero(long id, HeroRequest heroUpdateRequest) throws Exception {
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
     if (!StringUtils.hasLength(heroUpdateRequest.getName())) {
       // TODO: Change for HeroException later
       throw new Exception(MessagesConstants.HERO_EMPTY_NAME_ERROR);
@@ -63,7 +98,13 @@ public class HeroesService {
     return HeroDTO.buildHeroDTO(updatedHero);
   }
 
+  @Cacheable("search")
   public List<HeroDTO> getHeroesByName(String name) throws Exception {
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
     if (!StringUtils.hasLength(name)) {
       // TODO: Change for HeroException later
       throw new Exception(MessagesConstants.HERO_EMPTY_NAME_ERROR);

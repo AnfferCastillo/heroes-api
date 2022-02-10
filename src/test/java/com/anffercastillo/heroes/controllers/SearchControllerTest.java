1662b78514a76cb0b46e9098eed204b540c260b5
Anffer Castillo <anffer.castillo@gmail.com>
2022-02-09 23:29:05 -0300

    Testing search controller

--- src/test/java/com/anffercastillo/heroes/controllers/SearchControllerTest.java
+++ src/test/java/com/anffercastillo/heroes/controllers/SearchControllerTest.java
@@ -1,31 +1,49 @@
 package com.anffercastillo.heroes.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
 import com.anffercastillo.heroes.dto.SearchResponse;
 import com.anffercastillo.heroes.services.HeroesService;
 import com.anffercastillo.heroes.utils.HeroTestsUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@WebMvcTest(SearchController.class)
+@SpringBootTest
 public class SearchControllerTest {
-  @Autowired private MockMvc mockMvc;
+  private MockMvc mockMvc;
+
+  @Autowired private WebApplicationContext context;
 
   @Autowired ObjectMapper objectMapper;
 
   @MockBean private HeroesService mockHeroService;
 
+  @BeforeEach
+  public void setup() {
+    mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+  }
+
   @Test
+  @WithMockUser
   public void searchHeroesByName_Test() throws Exception {
     var name = "DUMMY";
     var dummyHero = HeroTestsUtils.buildDummyHero(1L);
@@ -50,8 +68,15 @@ public class SearchControllerTest {
   }
 
   @Test
+  @WithMockUser
   public void searchHeroesByName_Bad_Request_Test() throws Exception {
     mockMvc.perform(get("/search")).andExpect(status().isBadRequest());
     mockMvc.perform(get("/search?term=")).andExpect(status().isBadRequest());
+  }
+
+  @Test
+  @WithAnonymousUser
+  public void searchHeroesByName_Unauthorized_Test() throws Exception {
+    mockMvc.perform(get("/search?term=test")).andExpect(status().isUnauthorized());
   }
 }

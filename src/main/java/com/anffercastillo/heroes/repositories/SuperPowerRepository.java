ebf5693aba072dc9690dcfd33f9ec5d9c846cbeb
Anffer Castillo <anffer.castillo@gmail.com>
2022-02-11 18:56:23 -0300

    SuperPowerRepository SuperPowerRepositoryImpl
    - findSuperPowersByHeroId_Test
    - updateHeroesSuperPowers_Test

--- src/main/java/com/anffercastillo/heroes/repositories/SuperPowerRepository.java
+++ src/main/java/com/anffercastillo/heroes/repositories/SuperPowerRepository.java
@@ -1,10 +1,15 @@
 package com.anffercastillo.heroes.repositories;
 
-import java.util.List;
-
 import com.anffercastillo.heroes.dto.SuperPowerDTO;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface SuperPowerRepository {
 
   List<SuperPowerDTO> findSuperPowerByHeroId(long id);
+
+  List<SuperPowerDTO> updateHeroSuperPowers(long heroId, List<SuperPowerDTO> superPowes);
+
+  Optional<SuperPowerDTO> findById(long id);
 }

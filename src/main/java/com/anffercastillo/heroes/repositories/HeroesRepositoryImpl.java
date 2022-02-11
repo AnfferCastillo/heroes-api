59bfde8ec491d98f4c91c6ac944edcf779b5ade9
Anffer Castillo <anffer.castillo@gmail.com>
2022-02-11 16:56:57 -0300

    HeroresRepository
    - saveHero

--- src/main/java/com/anffercastillo/heroes/repositories/HeroesRepositoryImpl.java
+++ src/main/java/com/anffercastillo/heroes/repositories/HeroesRepositoryImpl.java
@@ -1,20 +1,23 @@
 package com.anffercastillo.heroes.repositories;
 
+import com.anffercastillo.heroes.dto.HeroDTO;
+import com.anffercastillo.heroes.dto.HeroRowMapper;
+import com.anffercastillo.heroes.dto.SuperPowerDTO;
+import com.anffercastillo.heroes.exceptions.HeroesNotFoundException;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.stereotype.Repository;
-
-import com.anffercastillo.heroes.dto.HeroDTO;
-import com.anffercastillo.heroes.dto.HeroRowMapper;
-import com.anffercastillo.heroes.dto.SuperPowerDTO;
-
 @Repository
 public class HeroesRepositoryImpl implements HeroesRepository {
 
+  protected static final String GET_NEXT_ID = "select max(id) + 1 from heroes";
+  protected static final String SAVE_HERO =
+      "update heroes set (name, forename, company) values (:name, :forename, :company) where id = :id";
   // PROTECTED FOR TESTING PURPUSES
   protected static final String QUERY_BY_NAME =
       "select * from heroes where lower(name) like lower('%:name%')";
@@ -36,9 +39,7 @@ public class HeroesRepositoryImpl implements HeroesRepository {
 
   @Override
   public List<HeroDTO> findHeroesByName(String name) {
-    return jdbcTemplate
-        .query(QUERY_BY_NAME, Map.of("name", name), new HeroRowMapper())
-        .stream()
+    return jdbcTemplate.query(QUERY_BY_NAME, Map.of("name", name), new HeroRowMapper()).stream()
         .map(this::setHeroSuperPowers)
         .collect(Collectors.toList());
   }
@@ -55,9 +56,7 @@ public class HeroesRepositoryImpl implements HeroesRepository {
 
   @Override
   public List<HeroDTO> findAll() {
-    return jdbcTemplate
-        .query(QUERY_ALL, new HeroRowMapper())
-        .stream()
+    return jdbcTemplate.query(QUERY_ALL, new HeroRowMapper()).stream()
         .map(this::setHeroSuperPowers)
         .collect(Collectors.toList());
   }
@@ -69,15 +68,30 @@ public class HeroesRepositoryImpl implements HeroesRepository {
 
   @Override
   public HeroDTO save(HeroDTO hero) {
-    // TODO Auto-generated method stub
-    return null;
+    var count =
+        jdbcTemplate.update(
+            SAVE_HERO,
+            Map.of(
+                "name",
+                hero.getName(),
+                "forename",
+                hero.getForename(),
+                "company",
+                hero.getCompany(),
+                "id",
+                hero.getId()));
+
+    // one row was updated
+    if (count == 1) {
+      return hero;
+    } else {
+      throw new HeroesNotFoundException();
+    }
   }
 
   private HeroDTO setHeroSuperPowers(HeroDTO dto) {
     var superPowers =
-        superPowersRepository
-            .findSuperPowerByHeroId(dto.getId())
-            .stream()
+        superPowersRepository.findSuperPowerByHeroId(dto.getId()).stream()
             .map(SuperPowerDTO::getDescription)
             .collect(Collectors.toList());
     dto.setSuperPowers(superPowers);

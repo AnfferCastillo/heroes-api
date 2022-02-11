ebf615872acc801d3856ceaa54b85d03b0b738aa
Anffer Castillo <anffer.castillo@gmail.com>
2022-02-11 18:55:28 -0300

    SuperPowerRepositoryTest
    - findSuperPowersByHeroId_Test
    - updateHeroesSuperPowers_Test

--- src/test/java/com/anffercastillo/heroes/repositories/SuperPowerRepositoryImplTest.java
+++ src/test/java/com/anffercastillo/heroes/repositories/SuperPowerRepositoryImplTest.java
@@ -5,18 +5,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 public class SuperPowerRepositoryImplTest {
 
-  private SuperPowerRepositoryImpl superPowerRepository;
+  private SuperPowerRepository superPowerRepository;
   private NamedParameterJdbcTemplate mockJdbcTemplate;
 
   @BeforeEach
@@ -45,19 +47,25 @@ public class SuperPowerRepositoryImplTest {
     var dummySuperPower = HeroTestsUtils.buildDummySuperPower(1L);
     var dummySuperPower2 = HeroTestsUtils.buildDummySuperPower(2L);
     var expected = List.of(dummySuperPower, dummySuperPower2);
-
-    mock(mockJdbcTemplate.query(
-            eq(SuperPowerRepositoryImpl.QUERY_BY_ID), eq(Map.of("id", 1L)), any(RowMapper.class)))
-        .thenReturn(dummySuperPower);
-    mock(mockJdbcTemplate.query(
-            eq(SuperPowerRepositoryImpl.QUERY_BY_ID), eq(Map.of("id", 2L)), any(RowMapper.class)))
-        .thenReturn(dummySuperPower2);
-
     var heroId = 1L;
     var superPowers = List.of(dummySuperPower, dummySuperPower2);
 
+    when(mockJdbcTemplate.queryForObject(
+            eq(SuperPowerRepositoryImpl.QUERY_BY_ID), eq(Map.of("id", 1L)), any(RowMapper.class)))
+        .thenReturn(dummySuperPower);
+    when(mockJdbcTemplate.queryForObject(
+            eq(SuperPowerRepositoryImpl.QUERY_BY_ID), eq(Map.of("id", 2L)), any(RowMapper.class)))
+        .thenReturn(dummySuperPower2);
+
+    when(mockJdbcTemplate.queryForObject(
+            eq(SuperPowerRepositoryImpl.COUNT_SUPER_POWERS), any(Map.class), any(Class.class)))
+        .thenReturn(2L);
+
+    when(mockJdbcTemplate.batchUpdate(
+            eq(SuperPowerRepositoryImpl.INSERT_HERO_SUPER_POWER), any(SqlParameterSource[].class)))
+        .thenReturn(new int[] {1, 1});
+
     var actual = superPowerRepository.updateHeroSuperPowers(heroId, superPowers);
-    assertEquals(expected.size(), actual.size());
-    assertEquals(expected.get(0), actual.get(0));
+    assertTrue(expected.containsAll(actual));
   }
 }
